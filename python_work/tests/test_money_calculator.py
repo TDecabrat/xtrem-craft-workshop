a2695a5659eb7c0ebfb581d8b61944315ca06232
unknown <o-but3@info-p-lp-30.iut.bx>
2023-10-10 09:13:53 +0200

    res float

--- python_work/tests/test_money_calculator.py
+++ python_work/tests/test_money_calculator.py
@@ -9,8 +9,8 @@ class TestMoneyCalculator:
     def test_add_in_usd_returns_value(self):
         """the test to add money from a different currency"""
         #Arrange
-        amount : Money = Money(5, Currency.USD)
-        amount2 : Money = Money(10, Currency.USD)
+        amount : Money = Money(5.0, Currency.USD)
+        amount2 : Money = Money(10.0, Currency.USD)
 
         #Act
         res = MoneyCalculator.add(amount, amount2)
@@ -23,7 +23,7 @@ class TestMoneyCalculator:
     def test_multiply_in_euros_returns_positive_number(self):
         """the test for checking that multipling money works"""
         #Arrange
-        amount : Money = Money(5, Currency.EUR)
+        amount : Money = Money(5.0, Currency.EUR)
         amount2 : int = 2
 
         #Act
@@ -36,7 +36,7 @@ class TestMoneyCalculator:
         """the test for checking that dividing money works"""
         #Arrange
 
-        amount : Money = Money(4002, Currency.KRW)
+        amount : Money = Money(4002.0, Currency.KRW)
         amount2 : int = 4
 
         #Act
